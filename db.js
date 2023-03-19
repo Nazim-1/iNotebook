6b675e28a70c2f81af758ab1b4685934597d5f50
Nazim-1 <116425604+Nazim-1@users.noreply.github.com>
2023-03-19 20:16:54 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const mongoURI = "mongodb+srv://mymongoapp:mymongoappjs@cluster0.bpnais5.mongodb.net/iNotebook"
 
+mongoose.set("strictQuery", false);
 const connectToMongo = ()=>{
     mongoose.connect(process.env.MONGO_URL, ()=>{
         console.log("Connected to Mongo Successfully")
