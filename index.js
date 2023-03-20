a13845850ee1bfa6d335160511bd4a9964952c9f
Nazim-1 <116425604+Nazim-1@users.noreply.github.com>
2023-03-20 20:28:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 
-
-
 const connectToMongo = require('./db')
 const express = require('express')
 var cors = require('cors')
 const dotenv = require('dotenv')
 const path = require('path')
+
+// connect with application/server
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const MONGO_URL = "mongodb+srv://mymongoapp:mymongoappjs@cluster0.bpnais5.mongodb.net/iNotebook?retryWrites=true&w=majority";
+const client = new MongoClient(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 
 
@@ -15,6 +24,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json());
+
 //Available Routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
@@ -22,7 +32,7 @@ app.use('/api/notes', require('./routes/notes'))
 //static files
 app.use(express.static(path.join(__dirname, './build')))
 
-app.get('*', function(req, res){
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, './build/index.html'))
 })
 
