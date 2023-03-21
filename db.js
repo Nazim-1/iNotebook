14f3014a0e3fbd1012242aa2fe6f92946ee4d202
Nazim-1 <116425604+Nazim-1@users.noreply.github.com>
2023-03-21 12:23:11 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
-const MONGO_URL = "mongodb+srv://mymongoapp:mymongoappjs@cluster0.bpnais5.mongodb.net/iNotebook"
+const MONGO_URL = "mongodb+srv://mymongoapp:mymongoappjs@cluster0.bpnais5.mongodb.net/iNotebook?retryWrites=true&w=majority"
 
 mongoose.set("strictQuery", false);
 const connectToMongo = ()=>{
-    mongoose.connect(MONGO_URL, ()=>{
-        console.log("Connected to Mongo Successfully")
+    mongoose.connect(MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
     })
 }
 
